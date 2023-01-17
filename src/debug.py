aa5e8fa94f00b1cf89b9ac20d541b8e1fb55d735
Andantei <zaoyinji@gmail.com>
2023-01-16 19:43:56 -0800

    add hello world plugin

---
+++ src/debug.py
@@ -0,0 +1,5 @@
+from hello_world_plugin import HelloWorld
+from tuneflow_devkit import Debugger
+
+if __name__ == "__main__":
+    Debugger(HelloWorld).start()
---
+++ src/hello_world_plugin.py
@@ -0,0 +1,46 @@
+from tuneflow_py import TuneflowPlugin, Song
+from typing import Any
+
+
+class HelloWorld(TuneflowPlugin):
+    @staticmethod
+    def provider_id():
+        return "andantei"
+
+    @staticmethod
+    def plugin_id():
+        return "hello-world"
+
+    @staticmethod
+    def provider_display_name():
+        return {
+            "zh": "Andantei 行板",
+            "en": "Andantei"
+        }
+
+    @staticmethod
+    def plugin_display_name():
+        return {
+            "zh": "Hello World 插件",
+            "en": "Hello World Plugin"
+        }
+
+    @staticmethod
+    def plugin_description():
+        return {
+            "zh": "这是一个Hello World插件",
+            "en": "This is a hello world plugin."
+        }
+
+    @staticmethod
+    def allow_reset():
+        return False
+
+    def run(self, song: Song, params: dict[str, Any]):
+        """
+        Do nothing except printing "hello world".
+
+        You should be able to load all metadata of this plugin and
+        the song should remain unchanged after running this plugin.
+        """
+        print("Hello World!")
